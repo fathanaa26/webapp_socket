1717f7b04a19598cae5b0b06104d790d64a1def4
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-06-28 03:21:34 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
+import {Manager} from 'socket.io-client'
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const manager = new Manager("localhost:8880",{
+    autoConnect:true,
+    forceNew:true,
+    transports:['websocket'],
+    reconnection:true,
+    reconnectionDelay:5000
+})
+
+const socket = manager.socket("/")
 
 function App() {
+  const [data,setData] = useState();
+  const [connected,setConnected] = useState();
+
+  useEffect(()=>{
+    socket.on("connect",()=>{
+      setConnected(`[${socket.id}] Connected `)
+    })
+
+    socket.on("ev",(arg)=>{
+      setData(arg.random)
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +34,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{connected}</h1>
+        <h2>{data}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
